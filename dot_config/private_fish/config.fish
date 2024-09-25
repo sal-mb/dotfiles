3855afc1847ab9a80886e6ac5310fa39ed9f0a0f
kauu1 <samukbore99@gmail.com>
2024-09-25 10:27:26 -0300

    changing .clang-format

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,5 +3,3 @@ if status is-interactive
 end
 
 set fish_greeting
-
-fish_add_path /home/kau/.local/bin /home/kau/bin /home/kau/.juliaup/bin /usr/local/bin /usr/local/sbin /usr/bin /usr/sbin /opt/rocm/bin /opt/rocm/profiler/bin /opt/rocm/opencl/bin /opt/rocm/hip/bin
