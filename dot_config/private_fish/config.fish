5d3336428bb532e7509fc92586c26b0505ea2887
kauu1 <samukbore99@gmail.com>
2024-09-25 10:20:10 -0300

    nvim, fish and c++ linter and format

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set fish_greeting
+
+fish_add_path /home/kau/.local/bin /home/kau/bin /home/kau/.juliaup/bin /usr/local/bin /usr/local/sbin /usr/bin /usr/sbin /opt/rocm/bin /opt/rocm/profiler/bin /opt/rocm/opencl/bin /opt/rocm/hip/bin
